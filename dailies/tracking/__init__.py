5d9a3b07350fb0f2bc75b31d3f9fbedefe51247a
Paco Ramos Koch <vfxpaco85@p4cm4n.com>
2025-04-11 11:46:04 +0200

    Remove import of FlowTracking as it's redundant with ShotgunTracking

--- dailies/tracking/__init__.py
+++ dailies/tracking/__init__.py
@@ -1,12 +1,10 @@
 from .shotgun_tracking import ShotgunTracking
 from .ftrack_tracking import FtrackTracking
 from .kitsu_tracking import KitsuTracking
-from .flow_tracking import FlowTracking
 
 # If you want to create a list of all engines for convenience
 __all__ = [
     'ShotgunTracking',
     'FtrackTracking',
     'KitsuTracking',
-    'FlowTracking',
 ]
